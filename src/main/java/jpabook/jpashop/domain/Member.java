f838b2d85af75f3eea2432ce0cee5ecf3f4035e9
Min <rudals0326@gmail.com>
2022-08-25 04:00:32 +0900

    feat : 주문조회 기능 추가(API)

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +18,13 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+
     private String name;
 
     @Embedded//내장을 포함한다.
     private Address address;
 
+    @JsonIgnore//넌 빠져
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
