024b2e359c6b7ab8a972c6f32c68980e544e3d0f
Min <rudals0326@gmail.com>
2022-08-26 23:29:14 +0900

    feat : JPA 적용

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,46 +1,12 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    /*@PersistenceContext
-    private EntityManager em;
-    스프링 부트의 빠와를 맛보아랏! -> @RequiredArgsConstructor
-     */
-
-    private final EntityManager em;
-
-    public void save(Member member) {
-
-        em.persist(member);
-    }
-
-    public Member findOne(Long id) {
-        return em.find(Member.class, id);
-    }
-
-    public List<Member> findAll() {
-        /*List<Member> result = em.createQuery("select m from Member m", Member.class)
-                .getResultList();
-
-        return result;*/
-
-        return em.createQuery("select m from Member m", Member.class)
-                .getResultList();
-    }
-
-    public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name = :name", Member.class)
-                .setParameter("name", name)
-                .getResultList();
-    }
+//selec m from Member m where m.name
+    List<Member> findByName(String name);
 }
